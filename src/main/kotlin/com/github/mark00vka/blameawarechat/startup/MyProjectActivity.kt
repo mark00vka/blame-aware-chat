79c9a8b5eb5e0a65b858912b5c013d0248093ec8
GitHub Action <action@github.com>
2026-04-13 19:48:51 +0200

    Template cleanup

--- src/main/kotlin/com/github/mark00vka/blameawarechat/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/mark00vka/blameawarechat/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.mark00vka.blameawarechat.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
